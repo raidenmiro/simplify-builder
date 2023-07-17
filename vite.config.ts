94e948f5954a0828195721ff4ea23114259c27d7
lordszn3 <robertkuzhin@gmail.com>
2023-07-17 15:17:30 +0500

    :heavy_plus_sign: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [tsconfigPaths(), preact()],
+  test: {
+    globals: true,
+  },
 });
